4b9d0f39420577f5d1561da06ebeb281e3881415
gabros20 <t.gabor80312@gmail.com>
2024-01-12 11:04:55 +0100

    fix: wrong filename

--- src/datas/build/seoContent.js
+++ src/datas/build/seoContent.js
@@ -2,5 +2,5 @@ export const seoContent = {
 	title: "Developer Portal | celestia.org",
 	ogTitle: "Developer Portal | celestia.org",
 	description: "The homepage for Celestia developers. Tutorials. Resources. Community.",
-	image: "/build-og-image.jpg",
+	image: "/developer-portal-og-image.jpg",
 };
--- src/datas/ecosystem/seoContent.js
+++ src/datas/ecosystem/seoContent.js
@@ -2,5 +2,5 @@ export const seoContent = {
 	title: "Ecosystem | celestia.org",
 	ogTitle: "Ecosystem | celestia.org",
 	description: "Celestia Ecosystem provides a wide range of apps and services built in the Celestia ecosystem.",
-    image: "/ecosystem-og-image.jpg",
+	image: "/ecosystem-og-image.jpg",
 };
