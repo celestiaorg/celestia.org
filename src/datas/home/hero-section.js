13212d0d2cc74b50562e73862195fc24703cc877
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2024-02-22 17:12:20 +0100

    Update hero-section.js

--- src/datas/home/hero-section.js
+++ src/datas/home/hero-section.js
@@ -1,6 +1,6 @@
 export const heroData = {
 	title: "The first modular blockchain network",
-	text: "Celestia is a modular data availability network that securely scales with the number of users, making it easy for anyone to launch their own blockchain.",
+	text: "Celestia is a modular data availability network that makes it easy for anyone to securely launch their own blockchain.",
 	buttons: [
 		{
 			text: "Build modular",
