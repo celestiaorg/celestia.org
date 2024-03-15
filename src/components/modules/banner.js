60ec68a82e8486e7e639cc3da16f8a8c033c158a
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2024-03-15 18:39:33 -0300

    Update banner.js

--- src/components/modules/banner.js
+++ src/components/modules/banner.js
@@ -32,7 +32,7 @@ export default class Banner extends React.Component {
                     {/* <div className={'col-2 position-relative d-none d-md-flex'}>
                         <Image alt={'Modular Summit'} filename={'modular-summit.svg'} />
                     </div> */}
-                    <div className={'col-auto'}>The first Celestia hackathon is here! Infinite Space Bazaar begins on April 2 <div style={{fontSize: `32px`, display: `inline-block`, transform: `translateY(4px)` }}>☄️</div></div>
+                    <div className={'col-auto'}>Get ready for the first Celestia hackathon! Infinite Space Bazaar begins on April 2 <div style={{fontSize: `32px`, display: `inline-block`, transform: `translateY(4px)` }}>☄️</div></div>
 
                     <a className={'col-auto mt-3 mt-sm-0'} href="https://genesis.celestia.org" target={'_blank'} rel={'noreferrer'}>
                         <div>
