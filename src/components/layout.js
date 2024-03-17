b9bce8f259e939a296c0d35798c419cb635ad4df
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2024-03-17 18:22:28 -0300

    banner (#385)
    * Update layout.js
    
    * Update banner.js
    
    * Update banner.js
    
    * Update banner.js
    
    * Update banner.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
+import Banner from "./modules/banner";
 
 export default function Layout({ children, footerBoxes, footerBoxes2 }) {
 	const [loaded, setLoaded] = useState(false);
@@ -32,6 +33,7 @@ export default function Layout({ children, footerBoxes, footerBoxes2 }) {
 
 	return (
 		<>
+			<Banner/>
 			<Header />
 			{children}
 			<Footer FooterBoxes={footerBoxes} FooterBoxes2={footerBoxes2} />
