5d099e68bf18d90db3de67100702c98edf9c01ea
gabros20 <t.gabor880312@gmail.com>
2024-03-19 11:39:03 +0100

    update: text size, backgroud, position, button size

--- src/components/modules/banner.js
+++ src/components/modules/banner.js
@@ -30,9 +30,10 @@ export default class Banner extends React.Component {
 					<i className={"icon-close"}></i>
 				</div>
 				<div className={"container"}>
-					<div className={"gap-3 justify-content-center align-items-center d-flex flex-column flex-lg-row"}>
+					<div className={"gap-2 gap-lg-4 justify-content-center align-items-center d-flex flex-column flex-lg-row"}>
 						<div className={"banner-text col-auto"}>
-							Get ready for the first Celestia hackathon! Infinite Space Bazaar begins on April 2 <div className={`emoji`}>☄️</div>
+							Get ready for the first Celestia hackathon! Infinite Space Bazaar begins on April 2
+							{/* <div className={`emoji`}>☄️</div> */}
 						</div>
 
 						<a className={"col-auto"} href='https://bazaar.celestia.org' target={"_blank"} rel={"noreferrer"}>
