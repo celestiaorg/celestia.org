5cd4632634ea32aaf5bcc88cc719820e398eacc7
Kyle Kelley <kowkelley@gmail.com>
2024-07-03 14:03:45 -0400

    Added Untitled Sans font.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
 import { Inter } from "next/font/google";
 import Nav from "@/components/Nav/Nav";
 import Footer from "@/components/Footer/Footer";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "./styles/fonts.css";
+import "./styles/globals.css";
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={``}>
         <Nav />
         {children}
         <Footer />
