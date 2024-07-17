369c456c5179393ba431b0bb40e446cb2509898c
Kyle Kelley <kowkelley@gmail.com>
2024-07-17 11:55:58 -0400

    Init carousel pagination

--- src/components/Blog/BlogCarousel.js
+++ src/components/Blog/BlogCarousel.js
@@ -7,6 +7,8 @@ import "./BlogCarousel.scss";
 import Slider from "react-slick";
 import { useRef, useState, useEffect } from "react";
 import Container from "../Container/Container";
+import Icon from "@/macros/Icons/Icon";
+import ArrowLongSVG from "@/macros/SVGs/ArrowLongSVG";
 
 const BlogCarousel = ({ children }) => {
   const sliderRef = useRef(null);
@@ -51,20 +53,14 @@ const BlogCarousel = ({ children }) => {
       const width = window.innerWidth;
       if (width < 768) {
         setSlidesToShow(1);
-      } else if (width < 1440) {
-        setSlidesToShow(2);
       } else {
         setSlidesToShow(2);
       }
     };
 
-    // Set initial value
     handleResize();
-
-    // Add event listener
     window.addEventListener("resize", handleResize);
 
-    // Cleanup event listener
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -78,33 +74,56 @@ const BlogCarousel = ({ children }) => {
       <Slider ref={sliderRef} {...settings}>
         {children}
       </Slider>
-      <button
-        className="p-10 border border-black block"
-        onClick={() => sliderRef.current.slickPrev()}
-      >
-        Previous
-      </button>
-      <div className="flex justify-center items-center space-x-2 mt-4">
-        {Array.from({ length: totalPages }).map((_, index) => (
+      <div className={"table mx-auto"}>
+        <div className="inline-flex items-center gap-4 mt-6 mb-10">
           <button
-            key={index}
-            className={`p-2 border border-black ${
-              currentPage === index + 1 ? "bg-gray-500 text-white" : ""
-            }`}
-            onClick={() =>
-              sliderRef.current.slickGoTo(index * settings.slidesToShow)
-            }
+            className="group"
+            onClick={() => sliderRef.current.slickPrev()}
           >
-            {index + 1}
+            <Icon
+              Icon={<ArrowLongSVG />}
+              hover
+              HoverIcon={<ArrowLongSVG />}
+              className={`flex-grow-0`}
+              direction={`left`}
+              border
+              size={"md"}
+            />
+            <span className={"sr-only"}>Previous Slide</span>
           </button>
-        ))}
+          <div className="flex justify-center items-center space-x-2">
+            {Array.from({ length: totalPages }).map((_, index) => (
+              <button
+                key={index}
+                className={`p-2 border border-black ${
+                  currentPage === index + 1 ? "bg-gray-500 text-white" : ""
+                }`}
+                onClick={() =>
+                  sliderRef.current.slickGoTo(index * settings.slidesToShow)
+                }
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+
+          <button
+            className="group"
+            onClick={() => sliderRef.current.slickNext()}
+          >
+            <Icon
+              Icon={<ArrowLongSVG />}
+              hover
+              HoverIcon={<ArrowLongSVG />}
+              className={`flex-grow-0`}
+              direction={`right`}
+              border
+              size={"md"}
+            />
+            <span className={"sr-only"}>Next Slide</span>
+          </button>
+        </div>
       </div>
-      <button
-        className="p-10 border border-black block"
-        onClick={() => sliderRef.current.slickNext()}
-      >
-        Next
-      </button>
     </Container>
   );
 };
