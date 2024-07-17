b58fd75698bd8cbbb14ba9bf1b955459345c4bdc
Kyle Kelley <kowkelley@gmail.com>
2024-07-16 20:38:18 -0400

    Explore cards hover icon

--- src/utils/rotateClassName.js
+++ src/utils/rotateClassName.js
@@ -1,9 +1,11 @@
 // return rotate class name based on direction
 export const rotateClass = (direction) => {
   switch (direction) {
+    case "top":
     case "up":
       return "rotate-0";
 
+    case "top-right":
     case "up-right":
     case "right-up":
       return "rotate-45";
@@ -11,13 +13,16 @@ export const rotateClass = (direction) => {
     case "right":
       return "rotate-90";
 
+    case "bottom-right":
     case "down-right":
     case "right-down":
       return "rotate-[135deg]";
 
+    case "bottom":
     case "down":
       return "rotate-180";
 
+    case "bottom-left":
     case "down-left":
     case "left-down":
       return "-rotate-[135deg]";
@@ -25,6 +30,7 @@ export const rotateClass = (direction) => {
     case "left":
       return "-rotate-90";
 
+    case "top-left":
     case "up-left":
     case "left-up":
       return "-rotate-45";
