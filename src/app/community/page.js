437ff121cdeed77bcf8d7b5a61f12aecaa82d484
Kyle Kelley <kowkelley@gmail.com>
2024-07-23 15:13:51 -0400

    Community page clean up

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -1,6 +1,3 @@
-import { socialChannels } from "@/data/community/socialChannels";
-import { explore } from "@/data/community/explore";
-import { ecosystem } from "@/data/community/ecosystem";
 import SecondaryHero from "@/components/Heroes/SecondaryHero";
 import Container from "@/components/Container/Container";
 import { Col, Row } from "@/macros/Grids";
@@ -8,58 +5,6 @@ import { Display, Body } from "@/macros/Copy";
 import IconCard from "@/components/Cards/IconCards/IconCard";
 
 export default async function Community() {
-  // return (
-  //   <main className={`flex min-h-screen flex-col p-24`}>
-  //     {/* HERO */}
-  //     <div className={`pb-10`}>
-  //       <h1 className={``}>Celestia Community</h1>
-  //       <p className={``}>Discover community hubs, discussion forums, and resources that are used by the global Celestia community.</p>
-  //     </div>
-
-  //     <hr />
-
-  //     {/* SOCIAL CHANNELS */}
-  //     <div className={`pb-10`}>
-  //       <h2 className={``}>{socialChannels.title}</h2>
-  //       <p>{socialChannels.description}</p>
-  //       <div className={``}>
-  //         {socialChannels.items.map((item, index) => (
-  //           <a className={"block"} key={index} href={item.url}>
-  //             <h3>{item.title}</h3>
-  //             <p>{item.text}</p>
-  //           </a>
-  //         ))}
-  //       </div>
-  //     </div>
-
-  //     <hr />
-
-  //     {/* EXPLORE */}
-  //     <div className={`pb-10`}>
-  //       <h2 className={``}>{explore.title}</h2>
-  //       <p>{explore.description}</p>
-  //       <div className={``}>
-  //         {explore.items.map((item, index) => (
-  //           <a className={"block"} key={index} href={item.url}>
-  //             <h3>{item.title}</h3>
-  //             <p>{item.text}</p>
-  //           </a>
-  //         ))}
-  //       </div>
-  //     </div>
-
-  //     <hr />
-
-  //     {/* ECOSYSTEM */}
-  //     <div className={`pb-10`}>
-  //       <h2 className={``}>{ecosystem.title}</h2>
-  //       <p>{ecosystem.description}</p>
-  //       <a href={ecosystem.button.url} className={``}>{ecosystem.button.text}</a>
-  //     </div>
-
-  //   </main >
-  // );
-
   const socialGroups = [
     {
       cards: [
@@ -200,7 +145,7 @@ export default async function Community() {
         </Container>
       </section>
 
-      <section id={"explore"} className="bg-white-weak text-black">
+      <section id={"explore"} className="bg-white text-black">
         <Container size={"lg"} className={"py-10 lg:py-24"}>
           <Row className={"mb-6 lg:mb-16"}>
             <Col width={60}>
