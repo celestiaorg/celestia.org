3e4c97b21b782bb43bc5e6bcc9e43c177756bee5
jcstein <46639943+jcstein@users.noreply.github.com>
2024-10-08 12:04:51 -0400

    docs: change discord link

--- src/datas/build/community.js
+++ src/datas/build/community.js
@@ -1,28 +1,28 @@
 export const community = {
-	title: "The community",
-	description: "Join Celestia’s community of developers",
-	items: [
-		{
-			id: 1,
-			title: "Discord",
-			text: "Have technical questions? Ask the developer community on Discord.",
-			image: "build/discord.png",
-			type: "external",
-			link: {
-				text: "Join Discord",
-				url: "https://discord.com/invite/YsnTPcSfWQ",
-			},
-		},
-		{
-			id: 2,
-			title: "Modular Meetup",
-			text: "Host your own grassroots meetup about modular blockchains.",
-			image: "build/forum.png",
-			type: "external",
-			link: {
-				text: "Host now",
-				url: "https://docs.celestia.org/community/modular-meetup-intro/",
-			},
-		},
-	],
+  title: "The community",
+  description: "Join Celestia’s community of developers",
+  items: [
+    {
+      id: 1,
+      title: "Discord",
+      text: "Have technical questions? Ask the developer community on Discord.",
+      image: "build/discord.png",
+      type: "external",
+      link: {
+        text: "Join Discord",
+        url: "https://discord.gg/celestiacommunity",
+      },
+    },
+    {
+      id: 2,
+      title: "Modular Meetup",
+      text: "Host your own grassroots meetup about modular blockchains.",
+      image: "build/forum.png",
+      type: "external",
+      link: {
+        text: "Host now",
+        url: "https://docs.celestia.org/community/modular-meetup-intro/",
+      },
+    },
+  ],
 };
