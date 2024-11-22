4915db97ac35e7c3c462a5109727c4115fef8e2a
Kyle Kelley <kowkelley@gmail.com>
2024-11-22 15:49:42 -0500

    init tia page design

--- src/app/what-is-tia/page.js
+++ src/app/what-is-tia/page.js
@@ -1,27 +1,16 @@
+import SecondaryHero from "@/components/Heroes/SecondaryHero";
+import Container from "@/components/Container/Container";
+import { Col, Row } from "@/macros/Grids";
+import { Display, Body } from "@/macros/Copy";
+import VerticalTitleCard from "@/components/Cards/VerticalTitleCards/VerticalTitleCard";
+import ProjectFilter from "@/components/ProjectFilter/ProjectFilter";
+import GetInTouch from "@/components/CallToActions/GetInTouch";
+import { getFilterOptions } from "@/utils/getFilterOptions";
+
 import meta from "@/components/Meta/Meta";
 import seo from "@/data/what-is-tia/seo";
 
 export const metadata = meta(seo);
-
-const heroData = {
-  title: "What is Tia?",
-  text: "Learn about Tia, the native token of the Celestia blockchain.",
-  buttons: [
-    {
-      text: "Pay for Data",
-      class:
-        "simple plausible-event-name=Pay_for_Data_button-What_is_TIA-Hero_section",
-      type: "external",
-      url: "https://docs.celestia.org/developers/submit-data/",
-    },
-    {
-      text: "Stake Tia",
-      class: "white",
-      type: "anchor",
-      url: "staking-and-custody",
-    },
-  ],
-};
 
 const role = {
   title: "Tia's role in Celestia",
@@ -29,42 +18,26 @@ const role = {
     {
       id: 1,
       title: "Pay for blobspace",
-      text: "Rollups pay to publish data to Celestia’s blobspace using Tia.",
+      description:
+        "Rollups pay to publish data to Celestia’s blobspace using Tia.",
       image: "what-is-tia/what-is-tia-role-1.png",
-      links: [
-        {
-          text: "Create wallet with celestia-node",
-          url: "https://docs.celestia.org/developers/celestia-node-key/",
-          type: "external",
-        },
-        {
-          text: "Learn more",
-          url: "https://docs.celestia.org/learn/paying-for-blobspace/",
-          type: "external",
-        },
-      ],
+      url: "https://docs.celestia.org/developers/celestia-node-key/",
+      // https://docs.celestia.org/learn/paying-for-blobspace/
     },
     {
       id: 2,
       title: "Secure the network",
-      text: "Users stake Tia to participate in consensus and secure Celestia.",
+      description:
+        "Users stake Tia to participate in consensus and secure Celestia.",
       image: "what-is-tia/what-is-tia-role-2.png",
-      type: "external",
-      links: [
-        {
-          text: "View documentation",
-          url: "https://docs.celestia.org/learn/tia/",
-          type: "external",
-        },
-      ],
+      url: "https://docs.celestia.org/learn/tia/",
     },
     {
       id: 3,
       title: "Gas token for rollups",
-      text: "Developers can use Tia as a gas token for their rollups.",
+      description: "Developers can use Tia as a gas token for their rollups.",
       image: "what-is-tia/what-is-tia-role-3.png",
-      type: "external",
-      links: [],
+      url: "",
     },
   ],
 };
@@ -74,97 +47,110 @@ const wallets = {
   details: "",
   items: [
     {
-      id: 1,
       title: "Keplr Wallet",
-      device: ["IOS", "Android", "Desktop"],
-      image: "what-is-tia/kepler-icon.jpg",
-      backdropClass: "opacity-50",
+      categories: ["IOS", "Android", "Desktop"],
+      image: "/images/app/what-is-tia/kepler-icon.jpg",
       url: "https://www.keplr.app/",
-      type: "external",
     },
     {
-      id: 2,
       title: "Cosmostation",
-      device: ["IOS", "Android", "Desktop"],
-      image: "what-is-tia/cosmostation-icon.jpg",
-      backdropClass: "opacity-100",
+      categories: ["IOS", "Android", "Desktop"],
+      image: "/images/app/what-is-tia/cosmostation-icon.jpg",
       url: "https://cosmostation.io/products/cosmostation_extension/",
-      type: "external",
     },
     {
-      id: 3,
       title: "Leap",
-      device: ["IOS", "Android", "Desktop"],
-      image: "what-is-tia/leap-icon.jpg",
-      backdropClass: "opacity-100",
+      categories: ["IOS", "Android", "Desktop"],
+      image: "/images/app/what-is-tia/leap-icon.jpg",
       url: "https://www.leapwallet.io/",
-      type: "external",
     },
     {
-      id: 4,
       title: "Ledger",
-      device: ["IOS", "Android", "Desktop"],
-      image: "what-is-tia/ledger-icon.jpg",
-      backdropClass: "opacity-50",
+      categories: ["IOS", "Android", "Desktop"],
+      image: "/images/app/what-is-tia/ledger-icon.jpg",
       url: "https://www.ledger.com/",
-      type: "external",
     },
   ],
 };
 
 export default async function WhatIsTia() {
   return (
-    <main className={`flex min-h-screen flex-col p-24`}>
-      {/* HERO */}
-      <div className={`pb-10`}>
-        <h1 className={``}>{heroData.title}</h1>
-        <p className={``}>{heroData.text}</p>
-        {heroData.buttons.map((button, index) => (
-          <a key={index} href={button.url}>
-            {button.text}
-          </a>
-        ))}
+    <>
+      <SecondaryHero
+        title={"What is Tia?"}
+        subtitle={
+          "Learn about Tia, the native token of the Celestia blockchain."
+        }
+        buttons={[
+          {
+            text: "Pay for Data",
+            url: "https://docs.celestia.org/developers/submit-data/",
+            iconDirection: "up-right",
+          },
+          {
+            text: "Stake Tia",
+            url: "/",
+            iconDirection: "down-right",
+          },
+        ]}
+      />
+
+      <section className="bg-black text-white">
+        <Container size={"lg"} className={"py-10 lg:py-24"}>
+          <Row className={"mb-6 lg:mb-16"}>
+            <Col width={60}>
+              <Display size={"sm"} tag={"h2"} className={"mb-4 lg:mb-0"}>
+                So, where should you start?
+              </Display>
+            </Col>
+            <Col width={40}>
+              <Body size={"md"}>
+                You can launch your chain as easily as a smart contract with
+                Celestia underneath. Here’s how you can start:
+              </Body>
+            </Col>
+          </Row>
+          <Row>
+            <Col width={100}>
+              <div
+                className={`w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4`}
+              >
+                {role.items.map((card, index) => (
+                  <VerticalTitleCard
+                    dark
+                    key={index}
+                    verticalTitle={card.title}
+                    description={card.description}
+                    url={card.url}
+                  />
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <div id="projects">
+        <ProjectFilter
+          title={"Wallets"}
+          description={
+            <>
+              Anyone can download a non-custodial wallet to freely control and
+              use their Tia.
+              <br />
+              <br />
+              It’s important to get familiar with basic wallet practices for
+              safely storing and transacting with Tia on Celestia.
+            </>
+          }
+          filters={getFilterOptions(wallets.items, "categories")}
+          filterTarget={"categories"}
+          items={wallets.items}
+          showCategoriesOnCard
+        />
       </div>
 
-      <hr />
-
-      {/* ROLE */}
-      <div className={`pb-10`}>
-        <h2>{role.title}</h2>
-        {role.items.map((item, index) => (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <p>{item.text}</p>
-            {item.links.map((link, index) => (
-              <a key={index} href={link.url} target="_blank" rel="noreferrer">
-                {link.text}
-              </a>
-            ))}
-          </div>
-        ))}
-      </div>
-
-      <hr />
-
-      {/* WALLETS */}
-      <div className={`pb-10`}>
-        <h2>Wallets</h2>
-        <p>
-          Anyone can download a non-custodial wallet to freely control and use
-          their Tia.
-        </p>
-        <p>
-          It’s important to get familiar with basic wallet practices for safely
-          storing and transacting with Tia on Celestia.
-        </p>
-        {wallets.items.map((item, index) => (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <p>Available on: {item.device.join(", ")}</p>
-            <a href={item.url}>Download</a>
-          </div>
-        ))}
-      </div>
-    </main>
+      <GetInTouch />
+    </>
   );
 }
