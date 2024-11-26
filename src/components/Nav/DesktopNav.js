ad0530897b27f15d385df4bd25b7f0760465f7d2
Kyle Kelley <kowkelley@gmail.com>
2024-11-26 16:24:01 -0500

    wip

--- src/components/Nav/DesktopNav.js
+++ src/components/Nav/DesktopNav.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import MenuData from "./data";
 import PrimaryButton from "@/macros/Buttons/PrimaryButton";
+import { Body } from "@/macros/Copy";
+import Link from "@/macros/Link/Link";
+import { motion, AnimatePresence } from "framer-motion";
 
 const DesktopNav = () => {
   const [activeDropdown, setActiveDropdown] = useState(null);
+
   return (
     <nav className="hidden lg:flex ml-auto mr-0">
       <ul className="flex gap-4 items-center w-full">
         {MenuData.map((menu, index) => (
-          <li key={menu.name} className="flex-grow-1">
+          <li key={menu.name} className="flex-grow-1 relative">
             <PrimaryButton
               href={menu.href}
               size="md"
@@ -20,22 +24,30 @@ const DesktopNav = () => {
               {menu.name}
             </PrimaryButton>
             {activeDropdown === index && (
-              <div className="absolute top-full left-0 w-full bg-white">
-                <ul className="flex flex-col gap-4 p-4">
-                  {menu.items.map((item) => (
-                    <li key={item.name}>
-                      <PrimaryButton
-                        href={item.href}
-                        size="sm"
-                        className={"bg-transparent text-black"}
-                        border={false}
-                      >
-                        {item.name}
-                      </PrimaryButton>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              <AnimatePresence>
+                <motion.div
+                  initial={{ opacity: 0, y: 30, x: "-50%" }}
+                  animate={{ opacity: 1, y: 0, y: 10 }}
+                  exit={{ opacity: 0, y: 30 }}
+                  className="absolute top-full left-1/2 w-64 bg-white-pure rounded-[.25rem] border-white-weak shadow-sm translate-y-2.5 -translate-x-1/2"
+                  onBlur={() => setActiveDropdown(null)}
+                >
+                  <ul className="flex flex-col p-4">
+                    {menu.items.map((item) => (
+                      <li key={item.name}>
+                        <Link
+                          href={item.url || ""}
+                          className={
+                            "text-black block p-2 hover:bg-white-weak focus:bg-white-weak rounded-[.25rem] transition"
+                          }
+                        >
+                          <Body size="sm">{item.name}</Body>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </motion.div>
+              </AnimatePresence>
             )}
           </li>
         ))}
