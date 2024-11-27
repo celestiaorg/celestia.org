536f6425a4f079a484ca596bad3e7cbb85611117
Kyle Kelley <kowkelley@gmail.com>
2024-11-27 12:11:36 -0500

    quick update to primary hero height

--- src/components/Heroes/PrimaryHero.js
+++ src/components/Heroes/PrimaryHero.js
@@ -5,7 +5,7 @@ import { Display, Body } from "@/macros/Copy";
 const PrimaryHero = ({ headline, subheadline, buttons, videos }) => {
   return (
     <section
-      className={`bg-white-weak relative lg:min-h-[550px] flex flex-col-reverse md:block`}
+      className={`bg-white-weak relative lg:min-h-[90vh] flex flex-col-reverse md:block`}
     >
       {videos && (
         <video
