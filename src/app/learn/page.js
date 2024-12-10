8b84db5843a793cfcbdfdea171a3a4d384394f9f
Kyle Kelley <kowkelley@gmail.com>
2024-12-10 13:55:53 -0500

    Swapped in correct Learn Module SEO data

--- src/app/learn/page.js
+++ src/app/learn/page.js
@@ -5,7 +5,7 @@ import { Display, Body } from "@/macros/Copy";
 import VerticalTitleCard from "@/components/Cards/VerticalTitleCards/VerticalTitleCard";
 
 import meta from "@/components/Meta/Meta";
-import seo from "@/data/faq/seo";
+import seo from "@/data/learn/seo";
 
 export const metadata = meta(seo);
 
