d309de8a31c8ecc7748c142d72f613f239c7ef4d
Gábor Tamás <t.gabor880312@gmail.com>
2025-01-07 15:38:40 +0100

    Updated event handling

--- src/components/Analytics/Plausible.js
+++ src/components/Analytics/Plausible.js
@@ -5,12 +5,7 @@ import Script from "next/script";
 export default function Plausible() {
 	return (
 		<>
-			<Script
-				defer
-				data-domain='celestia.org'
-				src='https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js'
-				strategy='afterInteractive'
-			/>
+			<Script defer data-domain='celestia.org' src='https://plausible.io/js/script.tagged-events.js' strategy='afterInteractive' />
 			<Script id='plausible-setup' strategy='afterInteractive'>
 				{`
           window.plausible = window.plausible || function() { 
