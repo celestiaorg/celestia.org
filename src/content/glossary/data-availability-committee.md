f4185309249216d60d78aa74c66cf03f314516ad
Gábor Tamás <t.gabor880312@gmail.com>
2025-01-20 11:32:01 +0100

    Updated stylling for links and better targeting

--- src/content/glossary/data-availability-committee.md
+++ src/content/glossary/data-availability-committee.md
@@ -3,4 +3,18 @@ title: "Data availability committee"
 description: "A data availability committee (DAC) is a permissioned group of nodes responsible for providing data availability to a blockchain."
 ---
 
-A data availability committee (DAC) is a permissioned group of nodes responsible for providing data availability to a blockchain.DAC’s are an insecure source of data availability because they make an honest majority assumption and don’t have any stake to slash if a data withholding attack is attempted. Data availability committees are primarily used to provide a cheap and less secure source of data availability to L2s.
+A data availability committee (DAC) is a permissioned group of nodes responsible for providing data availability to a blockchain. DAC's are an insecure source of data availability because they make an honest majority assumption and don't have any stake to slash if a data withholding attack is attempted. Data availability committees are primarily used to provide a cheap and less secure source of data availability to L2s.
+
+Here are some key points about DACs:
+
+-   They are **centralized** by nature
+-   They provide _cheaper_ but less secure data availability
+-   They rely on an [honest majority assumption](https://www.google.com/search?q=honest+majority+assumption+blockchain)
+
+## Security Considerations
+
+1. No slashing mechanism
+2. Potential for data withholding
+3. Trust assumptions
+
+### Example Code
--- src/macros/Copy/RichText.js
+++ src/macros/Copy/RichText.js
@@ -1,9 +1,9 @@
 const RichText = ({ children, className = "", size = "md" }) => {
 	const sizeClasses = {
-		xs: "text-xs [&>div>*]:leading-[1.2857]",
-		sm: "text-sm [&>div>*]:leading-[1.2857]",
-		md: "text-[1.0625rem] [&>div>*]:leading-[1.647]",
-		lg: "text-[1.0625rem] [&>div>*]:leading-[2rem] lg:text-2xl lg:[&>div>*]:leading-[2rem]",
+		xs: "text-xs [&_p]:leading-[1.2857]",
+		sm: "text-sm [&_p]:leading-[1.2857]",
+		md: "text-[1.0625rem] [&_p]:leading-[1.647]",
+		lg: "text-[1.0625rem] [&_p]:leading-[2rem] lg:text-2xl lg:[&_p]:leading-[2rem]",
 	};
 
 	return (
@@ -12,27 +12,27 @@ const RichText = ({ children, className = "", size = "md" }) => {
         ${sizeClasses[size]}
         ${className}
         text-pretty
-        [&>div>p]:mb-4 [&>div>p:last-child]:mb-0
-        [&>div>a]:text-blue-600 [&>div>a]:underline hover:[&>div>a]:text-blue-800
-        [&>div>strong]:font-bold
-        [&>div>em]:italic
-        [&>div>ul]:list-disc [&>div>ul]:ml-6 [&>div>ul]:mb-4
-        [&>div>ol]:list-decimal [&>div>ol]:ml-6 [&>div>ol]:mb-4
-        [&>div>li]:mb-2 [&>div>li:last-child]:mb-0
-        [&>div>h1]:text-4xl [&>div>h1]:font-bold [&>div>h1]:mb-6 [&>div>h1]:mt-8
-        [&>div>h2]:text-3xl [&>div>h2]:font-bold [&>div>h2]:mb-5 [&>div>h2]:mt-7
-        [&>div>h3]:text-2xl [&>div>h3]:font-bold [&>div>h3]:mb-4 [&>div>h3]:mt-6
-        [&>div>h4]:text-xl [&>div>h4]:font-bold [&>div>h4]:mb-4 [&>div>h4]:mt-6
-        [&>div>h5]:text-lg [&>div>h5]:font-bold [&>div>h5]:mb-3 [&>div>h5]:mt-5
-        [&>div>h6]:text-base [&>div>h6]:font-bold [&>div>h6]:mb-3 [&>div>h6]:mt-5
-        [&>div>blockquote]:border-l-4 [&>div>blockquote]:border-gray-300 [&>div>blockquote]:pl-4 [&>div>blockquote]:italic [&>div>blockquote]:my-6
-        [&>div>hr]:my-8 [&>div>hr]:border-t [&>div>hr]:border-gray-300
-        [&>div>code]:bg-gray-100 [&>div>code]:px-1 [&>div>code]:py-0.5 [&>div>code]:rounded
-        [&>div>pre]:bg-gray-100 [&>div>pre]:p-4 [&>div>pre]:rounded [&>div>pre]:mb-4
-        [&>div>img]:max-w-full [&>div>img]:h-auto [&>div>img]:rounded [&>div>img]:my-4
-        [&>div>table]:w-full [&>div>table]:mb-4 [&>div>table]:border-collapse
-        [&>div>th]:border [&>div>th]:border-gray-300 [&>div>th]:p-2 [&>div>th]:bg-gray-50
-        [&>div>td]:border [&>div>td]:border-gray-300 [&>div>td]:p-2
+        [&_p]:mb-4 [&_p:last-child]:mb-0
+        [&_a]:text-[#7b2bf9] [&_a]:underline [&_a]:underline-offset-4 [&_a]:decoration-1 hover:[&_a]:text-[#995DF9] hover:[&_a]:decoration-2 [&_a]:transition-all
+        [&_strong]:font-bold
+        [&_em]:italic
+        [&_ul]:list-disc [&_ul]:ml-6 [&_ul]:mb-4
+        [&_ol]:list-decimal [&_ol]:ml-6 [&_ol]:mb-4
+        [&_li]:mb-2 [&_li:last-child]:mb-0
+        [&_h1]:text-4xl [&_h1]:font-bold [&_h1]:mb-6 [&_h1]:mt-8
+        [&_h2]:text-3xl [&_h2]:font-bold [&_h2]:mb-5 [&_h2]:mt-7
+        [&_h3]:text-2xl [&_h3]:font-bold [&_h3]:mb-4 [&_h3]:mt-6
+        [&_h4]:text-xl [&_h4]:font-bold [&_h4]:mb-4 [&_h4]:mt-6
+        [&_h5]:text-lg [&_h5]:font-bold [&_h5]:mb-3 [&_h5]:mt-5
+        [&_h6]:text-base [&_h6]:font-bold [&_h6]:mb-3 [&_h6]:mt-5
+        [&_blockquote]:border-l-4 [&_blockquote]:border-gray-300 [&_blockquote]:pl-4 [&_blockquote]:italic [&_blockquote]:my-6
+        [&_hr]:my-8 [&_hr]:border-t [&_hr]:border-gray-300
+        [&_code]:bg-gray-100 [&_code]:px-1 [&_code]:py-0.5 [&_code]:rounded
+        [&_pre]:bg-gray-100 [&_pre]:p-4 [&_pre]:rounded [&_pre]:mb-4
+        [&_img]:max-w-full [&_img]:h-auto [&_img]:rounded [&_img]:my-4
+        [&_table]:w-full [&_table]:mb-4 [&_table]:border-collapse
+        [&_th]:border [&_th]:border-gray-300 [&_th]:p-2 [&_th]:bg-gray-50
+        [&_td]:border [&_td]:border-gray-300 [&_td]:p-2
       `}
 		>
 			{children}
