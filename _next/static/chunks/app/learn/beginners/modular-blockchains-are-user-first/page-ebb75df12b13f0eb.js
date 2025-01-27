e20c44a65f175cdb062a9dfb22119413a2629bd8
sysrex <sysrex@users.noreply.github.com>
2025-01-27 15:06:04 +0000

    deploy: cc536b2fb052b6360f9c6afbd499b32170929aa6

---
+++ _next/static/chunks/app/learn/beginners/modular-blockchains-are-user-first/page-ebb75df12b13f0eb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6381,4468,9732],{93406:function(e,n,s){Promise.resolve().then(s.bind(s,18832)),Promise.resolve().then(s.bind(s,14050))}},function(e){e.O(0,[6273,8173,1717,3989,2971,7023,1744],function(){return e(e.s=93406)}),_N_E=e.O()}]);
