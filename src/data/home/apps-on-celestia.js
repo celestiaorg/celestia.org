9e1973cb13546feb5581b9efec63053358d6c5a1
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2025-04-14 19:55:49 +0930

    Update apps-on-celestia.js

--- src/data/home/apps-on-celestia.js
+++ src/data/home/apps-on-celestia.js
@@ -8,17 +8,8 @@ export const appItems = [
 		chainIcon: "",
 		url: "https://x.com/AbstractChain/status/1882819498203545707/",
 	},
-	{
+    {
 		id: 2,
-		initialSlide: false,
-		title: "Derive",
-		description: "15 quarters of onchain options.",
-		image: "/images/app/homepage/apps-on-celestia/derive.jpg",
-		chainIcon: "",
-		url: "https://x.com/derivexyz/status/1907183394624258060/",
-	},
-	{
-		id: 3,
 		initialSlide: false,
 		title: "Eclipse",
 		description: "Eclipse consumes 10GB of Celestia DA per day.",
@@ -27,7 +18,7 @@ export const appItems = [
 		url: "https://x.com/nickwh8te/status/1879166596448702950/",
 	},
 	{
-		id: 4,
+		id: 3,
 		initialSlide: false,
 		title: "Towns",
 		description: "We’re building a future of onchain communication, where communities own their spaces and keep what they create.",
@@ -36,12 +27,21 @@ export const appItems = [
 		url: "https://x.com/TownsProtocol/status/1892273098457530734/",
 	},
 	{
-		id: 5,
+		id: 4,
 		initialSlide: false,
 		title: "Kinto",
 		description: "The Modular Exchange will break you free from gatekeepers.",
 		image: "/images/app/homepage/apps-on-celestia/kinto.jpg",
 		chainIcon: "",
 		url: "https://x.com/KintoXYZ/status/1905273925535400416/",
+	},
+    {
+		id: 5,
+		initialSlide: false,
+		title: "Derive",
+		description: "15 quarters of onchain options.",
+		image: "/images/app/homepage/apps-on-celestia/derive.jpg",
+		chainIcon: "",
+		url: "https://x.com/derivexyz/status/1907183394624258060/",
 	},
 ];
