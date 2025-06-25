291842b4ebc149f84ba535761c0da883601e1e84
Alex Beckett <75361908+alex-beckett@users.noreply.github.com>
2025-06-25 17:23:32 +0800

    Update apps-on-celestia.js

--- src/data/home/apps-on-celestia.js
+++ src/data/home/apps-on-celestia.js
@@ -12,7 +12,7 @@ export const appItems = [
 		id: 2,
 		initialSlide: false,
 		title: "Hibachi",
-		description: "Hibachi—the first provable CLOB—has launched with Celestia's high-performance Private DA. With programmable privacy, trading activity remains confidential and publicly verifiable.",
+		description: "Hibachi—the first provable CLOB—launches with Celestia’s high-performance Private DA, enabling programmable privacy that keeps trading confidential and verifiable.",
 		image: "/images/app/homepage/apps-on-celestia/hibachi.jpg",
 		chainIcon: "",
 		url: "https://x.com/hibachi_xyz/status/1935009540094701743",
