7577410d8f8f8869b329110cea6b8d1b7fb02b5e
Gábor Tamás <t.gabor880312@gmail.com>
2025-09-19 16:19:57 +0200

    feat: updated page seo for planetary

--- src/app/planetary/page.js
+++ src/app/planetary/page.js
@@ -1,6 +1,3 @@
-import { Row, Col } from "@/macros/Grids";
-import Container from "@/components/Container/Container";
-import Image from "next/image";
 import PlanetaryClient from "./PlanetaryClient";
 
 import Meta from "@/components/Meta/Meta";
@@ -8,6 +5,6 @@ import seo from "@/data/mammoth/seo";
 
 export const metadata = Meta(seo);
 
-export default async function Mammoth() {
+export default async function Planetary() {
 	return <PlanetaryClient />;
 }
