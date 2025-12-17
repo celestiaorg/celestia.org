ae4ac093eac3b6a4af032a78d6f81eadfdd7cf51
jcstein <z@zs-MacBook-Air.local>
2025-12-17 10:41:16 -0700

    docs: remove projects that don't use celestia or don't exist

--- src/data/build/rollups.js
+++ src/data/build/rollups.js
@@ -1,94 +1,66 @@
 import { ANALYTICS_EVENTS } from "@/constants/analytics";
 
 export const rollups = [
-	{
-		title: "AltLayer",
-		categories: ["Arbitrum ORBIT", "OP Stack", "Polygon CDK"],
-		description: "We offer a versatile rollup stack including Arbitrum Orbit, OP stack, Polygon zkEVM and StarkWare.",
-		image: "/images/app/build/altlayer.png",
-		url: "https://altlayer.io/raas/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_ALTLAYER,
-	},
-	{
-		title: "Astria",
-		categories: [],
-		description: "Astria lets rollups share a single decentralized network of sequencers that's simple and permissionless to join.",
-		image: "/images/app/build/astria.png",
-		url: "https://www.astria.org/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_ASTRIA,
-	},
-	{
-		title: "Caldera",
-		categories: ["Arbitrum ORBIT", "OP Stack"],
-		description: "Caldera makes it easy to launch performant, customizable Arbitrum Orbit and OP Stack rollups. No code required.",
-		image: "/images/app/build/caldera.png",
-		url: "https://www.caldera.xyz/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_CALDERA,
-	},
-	{
-		title: "Conduit",
-		categories: ["Arbitrum ORBIT", "OP Stack"],
-		description:
-			"Deploy a rollup in a few clicks, no code required. Fully-managed, production grade OP Stack and Arbitrum Orbit rollups on Ethereum.",
-		image: "/images/app/build/conduit.png",
-		url: "https://conduit.xyz/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_CONDUIT,
-	},
-	{
-		title: "Gateway",
-		categories: ["Polygon CDK"],
-		description: "Create your own zkEVM with all the infrastructure you need.",
-		image: "/images/app/build/gateway.png",
-		url: "https://gateway.fm/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_GATEWAY,
-	},
-	{
-		title: "Gelato",
-		categories: ["Arbitrum ORBIT", "OP Stack", "Polygon CDK"],
-		description:
-			"The all-in-one Ethereum Rollup as a Service Platform. Deploy production-grade & fully-serviced L2 rollups natively integrated with tools like oracles, bridges, data indexers and Account Abstraction.",
-		image: "/images/app/build/gelato.png",
-		url: "https://www.gelato.network/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_GELATO,
-	},
-	{
-		title: "Karnot",
-		categories: ["Starknet Stack"],
-		description: "Karnot abstracts all the difficult parts of managing your own app chain so that you can build what really matters.",
-		image: "/images/app/build/karnot.png",
-		url: "https://www.karnot.xyz/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_KARNOT,
-	},
-	{
-		title: "Lumoz",
-		categories: ["Polygon CDK"],
-		description: "A decentralized ZK-RaaS network featuring ZKP mining.",
-		image: "/images/app/build/lumoz.png",
-		url: "https://lumoz.org/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_LUMOZ,
-	},
-	{
-		title: "Snapchain",
-		categories: ["Polygon CDK"],
-		description: "Leverage the power of ZK Rollups to scale your on-chain game or DeFi app cheaply and securely, without compromise.",
-		image: "/images/app/build/snapchain.png",
-		url: "https://snapchain.dev/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_SNAPCHAIN,
-	},
-	{
-		title: "Vistara",
-		categories: [],
-		description: "Vistara is building a Hardware availability layer for the modular web aiming to democratize access to decentralized hardware.",
-		image: "/images/app/build/vistara.png",
-		url: "https://docs.vistara.dev/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_VISTARA,
-	},
-	{
-		title: "Zeeve",
-		categories: ["Arbitrum ORBIT", "OP Stack", "Polygon CDK"],
-		description: "Build, Customize, and Launch your production-grade Rollups with Zeeve’s Rollups as a Service (RaaS).",
-		image: "/images/app/build/zeeve.png",
-		url: "https://www.zeeve.io/",
-		trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_ZEEVE,
-	},
+  {
+    title: "AltLayer",
+    categories: ["Arbitrum ORBIT", "OP Stack", "Polygon CDK"],
+    description:
+      "We offer a versatile rollup stack including Arbitrum Orbit, OP stack, Polygon zkEVM and StarkWare.",
+    image: "/images/app/build/altlayer.png",
+    url: "https://altlayer.io/raas/",
+    trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_ALTLAYER,
+  },
+  {
+    title: "Caldera",
+    categories: ["Arbitrum ORBIT", "OP Stack"],
+    description:
+      "Caldera makes it easy to launch performant, customizable Arbitrum Orbit and OP Stack rollups. No code required.",
+    image: "/images/app/build/caldera.png",
+    url: "https://www.caldera.xyz/",
+    trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_CALDERA,
+  },
+  {
+    title: "Conduit",
+    categories: ["Arbitrum ORBIT", "OP Stack"],
+    description:
+      "Deploy a rollup in a few clicks, no code required. Fully-managed, production grade OP Stack and Arbitrum Orbit rollups on Ethereum.",
+    image: "/images/app/build/conduit.png",
+    url: "https://conduit.xyz/",
+    trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_CONDUIT,
+  },
+  {
+    title: "Gateway",
+    categories: ["Polygon CDK"],
+    description: "Create your own zkEVM with all the infrastructure you need.",
+    image: "/images/app/build/gateway.png",
+    url: "https://gateway.fm/",
+    trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_GATEWAY,
+  },
+  {
+    title: "Gelato",
+    categories: ["Arbitrum ORBIT", "OP Stack", "Polygon CDK"],
+    description:
+      "The all-in-one Ethereum Rollup as a Service Platform. Deploy production-grade & fully-serviced L2 rollups natively integrated with tools like oracles, bridges, data indexers and Account Abstraction.",
+    image: "/images/app/build/gelato.png",
+    url: "https://www.gelato.network/",
+    trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_GELATO,
+  },
+  {
+    title: "Snapchain",
+    categories: ["Polygon CDK"],
+    description:
+      "Leverage the power of ZK Rollups to scale your on-chain game or DeFi app cheaply and securely, without compromise.",
+    image: "/images/app/build/snapchain.png",
+    url: "https://snapchain.dev/",
+    trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_SNAPCHAIN,
+  },
+  {
+    title: "Zeeve",
+    categories: ["Arbitrum ORBIT", "OP Stack", "Polygon CDK"],
+    description:
+      "Build, Customize, and Launch your production-grade Rollups with Zeeve’s Rollups as a Service (RaaS).",
+    image: "/images/app/build/zeeve.png",
+    url: "https://www.zeeve.io/",
+    trackEvent: ANALYTICS_EVENTS.PORTAL_RAAS_ZEEVE,
+  },
 ];
